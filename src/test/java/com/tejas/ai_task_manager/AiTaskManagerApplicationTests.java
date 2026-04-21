a9e6562947bf22674b7222f16457e5fcc04d592f
Tejas Patil <tejaspatil@Tejass-Mac.local>
2026-04-21 10:56:51 +0530

    initial setup

---
+++ src/test/java/com/tejas/ai_task_manager/AiTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejas.ai_task_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
